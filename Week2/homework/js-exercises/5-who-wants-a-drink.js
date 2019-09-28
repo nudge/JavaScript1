4642a6650de524d8ff12240d4545a4e509380e30
Ali Dagdeviren <54191107+adagdeviren@users.noreply.github.com>
2019-09-28 05:43:18 +0200

    working on details

--- Week2/homework/js-exercises/5-who-wants-a-drink.js
+++ Week2/homework/js-exercises/5-who-wants-a-drink.js
@@ -4,14 +4,21 @@
     const drinkTypes = ['cola', 'lemonade', 'water'];
     let drinkTray = [];
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; ++i) {
+
         if (i < drinkTypes.length) {
-          drinkTray.push(drinkTypes[i])
+
+          drinkTray.push(drinkTypes[i]);
         
         } else {
-          drinkTray.push(drinkTypes[i - drinkTypes.length])
+
+          drinkTray.push(drinkTypes[i - drinkTypes.length]);
         
         }
+        /*
+        right now it shows the result that we want, but it's not the correct way. 
+        we need a smarter logic to check drink types's quantity and change the behavior.
+        */ 
       }
       console.log("Hey guys, I brought a " + drinkTray.join(', ') + "!");
 }
