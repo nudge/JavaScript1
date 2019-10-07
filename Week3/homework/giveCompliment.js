7c5f319e8ba8277445e81ecbdd8eb55dec701270
Ali Amouri Kadhim <53454241+AliKdhim87@users.noreply.github.com>
2019-10-07 23:28:33 +0200

    Thank you for the feedback
    I changed the name of the variable

--- Week3/homework/giveCompliment.js
+++ Week3/homework/giveCompliment.js
@@ -3,10 +3,10 @@ const giveCompliment = (name) => {
     // Array with ten Complimen
     const complimentArray = ['great', 'awesome', 'great', 'nice', 'Elegant', 'Elegant', 'Majestic', 'Lovely', 'Striking', 'Amazing'];
     // to ronden the content of the array 
-    const randomArray = Math.floor(Math.random() * complimentArray.length);
+    const randomIndex = Math.floor(Math.random() * complimentArray.length);
 
-    return `You are  ${complimentArray[randomArray]} ${name}`
+    return `You are  ${complimentArray[randomIndex]} ${name}`
 }
 console.log(giveCompliment('Ali'));
-console.log(giveCompliment('Ali'));
-console.log(giveCompliment('Ali'));
+console.log(giveCompliment('Ali')); 
+console.log(giveCompliment('Ali'));randomArray
