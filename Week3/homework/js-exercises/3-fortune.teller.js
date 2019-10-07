9026dd1742419bb4d2fdd8fdfffe3d61e2301577
Sila Kaymak <54150648+bsilakaymak@users.noreply.github.com>
2019-10-07 21:49:13 +0200

    changed fortune teller exercise

--- Week3/homework/js-exercises/3-fortune.teller.js
+++ Week3/homework/js-exercises/3-fortune.teller.js
@@ -5,9 +5,12 @@ const partnerNames = [ "A", "B", "C", "D", "E"];
 const locations = ["Istanbul", "Amsterdam", "Paris","Diyarbakir", "Tokyo"];
 const jobs = ["waiter", "doctor", "lawyer", "painter", "singer"];
 const randomNumber = Math.floor(Math.random() * 5);
-
-function tellFortune(){
-  return "You will be a " + jobs[randomNumber] + " in " + locations[randomNumber] + ", and married to " +partnerNames[randomNumber] + " with " + numChildren[randomNumber] + " kids."
+function randomElement(array){
+ return array[Math.floor(Math.random() * array.length)];
 }
 
-console.log(tellFortune());
+function tellFortune(job, city, partner, kids){
+  return "You will be a " + randomElement(jobs) + " in " + randomElement(locations) + ", and married to " + randomElement(partnerNames) + " with " + randomElement(numChildren) + " kids."
+}
+
+console.log(tellFortune(jobs, locations, partnerNames, numChildren));
