8380a948cf8c209b221367a794353f496e6f4082
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-10-07 16:38:00 +0200

    Update 1-compliment.js

--- Week3/homework/1-compliment.js
+++ Week3/homework/1-compliment.js
@@ -1,14 +1,12 @@
- 
 'use strict'
-let name = prompt("Enter your beautiful name"); 
-  
- function givecomplement (name){
-	 
+
+ function givecomplements (name){
+
     const compliments=["great", "awesome","trusty","respectful", "friendly", "clever", "Generous", "brilliant", "handsome", "beautiful"];
-	 
-   return `You are ${compliments[Math.floor(Math.random() * compliments.length)]}, ${name}`;
+
+   return `You are ${compliments[Math.floor(Math.random() * compliments.length)]}, ${name}`+"!";
 }
 
-console.log(givecomplement(name));
-console.log(givecomplement(name));
-console.log(givecomplement(name));
+console.log(givecomplements("Muhammad"));
+console.log(givecomplements("Muhammad"));
+console.log(givecomplements("Muhammad"));
