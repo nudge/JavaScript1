6a8b62da04494643f0516ff9c9c9f9cfdf8e07d4
Sila Kaymak <54150648+bsilakaymak@users.noreply.github.com>
2019-10-07 21:22:58 +0200

    improved indentation

--- Week3/homework/js-exercises/5-total-cost.js
+++ Week3/homework/js-exercises/5-total-cost.js
@@ -3,7 +3,7 @@ function calculateTotalPrice(productsObject){
     let sum=0;
       for(let product in productObject ){
         sum += productsObject[product];}
-    return sum;
+      return sum;
 }
 const cartForParty = { haydari:10, acuka:8, raki:25, chips:5, cola:5, salgam:10, ice:2};
 
