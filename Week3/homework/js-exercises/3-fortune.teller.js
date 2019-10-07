f9641b8ed8f1fe683229acbcf7544ad7f94afed0
Sila Kaymak <54150648+bsilakaymak@users.noreply.github.com>
2019-10-07 20:57:19 +0200

    moved arrays outside of the function

--- Week3/homework/js-exercises/3-fortune.teller.js
+++ Week3/homework/js-exercises/3-fortune.teller.js
@@ -1,12 +1,12 @@
 "use strict";
 
-function tellFortune(numChildren, partnerNames, locations, jobs){
-  numChildren = [1, 2, 3, 4, 5];
-  partnerNames = [ "A", "B", "C", "D", "E"];
-  locations = ["Istanbul", "Amsterdam", "Paris","Diyarbakir", "Tokyo"];
-  jobs = ["waiter", "doctor", "lawyer", "painter", "singer"];
-  let randomNumber = Math.floor(Math.random() * 5);
+const numChildren = [1, 2, 3, 4, 5];
+const partnerNames = [ "A", "B", "C", "D", "E"];
+const locations = ["Istanbul", "Amsterdam", "Paris","Diyarbakir", "Tokyo"];
+const jobs = ["waiter", "doctor", "lawyer", "painter", "singer"];
+const randomNumber = Math.floor(Math.random() * 5);
 
+function tellFortune(){
   return "You will be a " + jobs[randomNumber] + " in " + locations[randomNumber] + ", and married to " +partnerNames[randomNumber] + " with " + numChildren[randomNumber] + " kids."
 }
 
