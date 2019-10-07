71fff94e1109f731b20992de5aace46d8d581e18
Ali Amouri Kadhim <53454241+AliKdhim87@users.noreply.github.com>
2019-10-07 23:00:47 +0200

    thank you for the feedback
    I changed

--- Week3/homework/calculateDogAge.js
+++ Week3/homework/calculateDogAge.js
@@ -1,8 +1,8 @@
 'use strict';
 function calculateDogAge(puppysAge) {
     const dogYears = 7 * puppysAge;
-    return console.log(`Your doggie is${dogYears} years old in dog years!`);
+    return `Your doggie is${dogYears} years old in dog years!`
 }
-calculateDogAge(1);
-calculateDogAge(0.5);
-calculateDogAge(2);
+console.log(calculateDogAge(1));
+console.log(calculateDogAge(0.5));
+console.log(calculateDogAge(2));
