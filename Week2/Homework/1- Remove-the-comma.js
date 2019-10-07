6f3bf32e2ac36d59bc2b962eb24b1b5833a892eb
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-10-07 12:51:46 +0200

    Update 1- Remove-the-comma.js

--- Week2/Homework/1- Remove-the-comma.js
+++ Week2/Homework/1- Remove-the-comma.js
@@ -1,9 +1,9 @@
 
 "use strict";
-let myString = 'hello,this,is,a,difficult,to,read,sentence';
+const myString = 'hello,this,is,a,difficult,to,read,sentence';
 
 console.log(myString.length);
 
-let myNewString = myString.replace(/,/g, ' ');
+const myNewString = myString.replace(/,/g, ' ');
 
 console.log(myNewString);
