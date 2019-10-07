7bf7a367eb4938b9cd26dc165f37ef7e40ad0b72
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-10-07 14:45:41 +0200

    Update Grade-calculator.js

--- Week2/Homework/Grade-calculator.js
+++ Week2/Homework/Grade-calculator.js
@@ -1,10 +1,8 @@
  
 
-  let score=prompt("Enter your score"); // Manual input
-  let full_Score=100;
-
+  
  // This function turns scores into percentage values.
- function percentage_calculator (studentScore, maxScore){
+ function percentage_calculator (score, full_Score){
     let studentscore = Math.round(( score / full_Score) * 100);
     return studentscore;
 }
@@ -12,37 +10,36 @@
 
 // This function shows the letter grade and score as percentage value.
 
-function grade_calculator (studentscore , studentGrade){
+function grade_calculator (studentscore){
  
-    if(studentscore >= 90) {
+    if(studentscore <= 100 && studentscore >= 90) {
         studentGrade = "A";
-        console.log(" You got an " + studentGrade + " ("+ studentscore + "%)");
+        console.log("You got an " + studentGrade + " ("+ studentscore + "%)");
     }
-    else if(studentscore <= 89 && studentscore >= 80) {
+    if(studentscore <= 89 && studentscore >= 80) {
         studentGrade = "B";
-        console.log(" You got a " + studentGrade +" ("+ studentscore + "%)");
+        console.log("You got a " + studentGrade +" ("+ studentscore + "%)");
     }
-    else if(studentscore <=79 && studentscore >= 70) {
+    if(studentscore <=79 && studentscore >= 70) {
         studentGrade = "C";
-        console.log(" You got a " + studentGrade +" ("+ studentscore + "%)");
+        console.log("You got a " + studentGrade +" ("+ studentscore + "%)");
     }
-    else if(studentscore <= 69 && studentscore >= 60) {
+    if(studentscore <= 69 && studentscore >= 60) {
         studentGrade ="D";
-        console.log(" You got a " + studentGrade + " ("+ studentscore + "%)");
+        console.log("You got a " + studentGrade + " ("+ studentscore + "%)");
     }
-    else if(studentscore <=59 && studentscore >= 50) { 
+    if(studentscore <=59 && studentscore >= 50) { 
         studentGrade = "E";
-        console.log(" You got an " + studentGrade + " ("+ studentscore + "%)");
+        console.log("You got an " + studentGrade + " ("+ studentscore + "%)");
     }
-    else if(studentscore <50 && studentscore >= 0) {
+    if(studentscore <50 && studentscore >= 0) {
         studentGrade = "F";
-        console.log(" You got an " + studentGrade +" ("+ studentscore + "%)");
+        console.log("You got an " + studentGrade +" ("+ studentscore + "%)");
     }
-   else{
-  console.log(" Enter a number between 0 and 100");
-
-   }
+   
 }
-grade_calculator(percentage_calculator(score,full_Score));
+grade_calculator(percentage_calculator(85, 100));
+grade_calculator(percentage_calculator(92, 100));
+grade_calculator(percentage_calculator(43, 100));
 
 
