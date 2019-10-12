f1d68a79231cb9839752a5cc24fb4b305ad6295f
nbayram <nbayram4@gmail.com>
2019-10-12 22:50:57 +0200

    change let keyword

--- Week3/homework/js-exercises/dogYears.js
+++ Week3/homework/js-exercises/dogYears.js
@@ -7,8 +7,9 @@
   // It takes 1 argument: your puppy's age (number).
   const calculateDogAge = yourPuppysAge => {
 
-    // Calculate your dog's age based on the conversion rate of 1 human year to 7 dog years.  
-    let dogYear = yourPuppysAge * 7;
+    // Calculate your dog's age based on the conversion rate of 1 human year to 7 dog years.
+    // const instead of let  
+    const dogYear = yourPuppysAge * 7;
 
     //Return a string: "Your doggie is [CALCULATED_VALUE] years old in dog years!"
     return `Your doggie is ${dogYear} years old in dog years!`;
