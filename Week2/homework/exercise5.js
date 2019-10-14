b9ff26e6b2a4c5abaa8c91b4e30b64c982ee3411
alineafpereira <aline.amaralds@gmail.com>
2019-10-14 21:35:18 +0200

    Update exercise5.js
    Tried to correct but isn't still perfect

--- Week2/homework/exercise5.js
+++ Week2/homework/exercise5.js
@@ -1,18 +1,18 @@
 'use strict'
 
 let drinkTray = [ ];
-const drinkTypes = ['cola','lemonade','water']
-console.log(drinkTypes) /*just tested if the array is working properly*/
+const drinkTypes = [' cola',' lemonade',' water'];
 
-for (let i = 0; drinkTray.length <= 5; i++) {
-    var idx = drinkTypes[Math.floor(Math.random() * drinkTypes.length)]; /*wanted to pick a random piece of the array*/
-    drinkTray.push(drinkTypes[idx]);
-    console.log(drinkTray);
-    break
+for (let i = 0; drinkTray.length < 5; i++) {
+    while(true){
+        let randomDrink = drinkTypes[Math.floor(Math.random())];
+        if (drinkTray.filter(drink => {
+            return drink === randomDrink
+        }).length < 2) {
+        drinkTray.push(randomDrink);
+        break
+        }
+   }
 }
 
-console.log("Hey guys, I brought" + drinkTray + "!")
-
-/*My expectation about the random var was that it would be really random so each time we run the code 
-it give us back different compositions to the drinkTray array.
-Another point that I still didn't find out is why there is this bunch of comas. */
+console.log(`Hey guys, I brought ${drinkTray}!`)
